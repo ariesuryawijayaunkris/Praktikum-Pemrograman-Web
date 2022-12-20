2f55729e4420b78bd229f17dad90facac49c58e5
ariesuryawijayaunkris <120257026+ariesuryawijayaunkris@users.noreply.github.com>
2022-12-20 10:46:44 +0700

    Add files via upload

---
+++ Praktikum Pemrograman Web_Arie Suryawijaya_2170234020/Pertemuan_4/latihan2_array.php
@@ -0,0 +1,12 @@
+<html>
+<head><title>Pemrograman PHP dengan Array</title></head>
+<body>
+<?php
+    $nama[]="Agung";
+    $nama[]="Bowo";
+    $nama[]="Alma";
+    echo $nama[1].$nama[2].$nama[0];
+    echo "<br>";
+?>
+</body>
+</html>
